c577dc267549b2f6197d29ae619fe84966564036
Mohammad Reza Eini <80261978+MR-Eini@users.noreply.github.com>
2024-08-09 00:59:18 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Simple console log for debugging
+console.log('Welcome to Mohammad Reza Eini\'s CV page!');
